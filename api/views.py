525c93dde3167dc5fa551cee6053151796027cb9
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 22:37:25 +0530

    Use pcalc module to evaluate given expressions

--- api/views.py
+++ api/views.py
@@ -2,23 +2,30 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
+from pcalc import VariablesTableType, Parser
+
 from .serializers import SyntaxValidator
+
+
+global_parser = Parser()
 
 
 class EvaluateView(CreateAPIView):
     serializer_class = SyntaxValidator
 
     def post(self, request):
+        expression = request.data.get('expression')
+        variables_dict = request.data.get('variables') or dict()
+        variables = VariablesTableType(variables_dict)
+
         try:
-            # result = eval(request.data.get('expression'))
-            # TODO: calculate the right result
-            result = 0
+            result = global_parser.evaluate(expression, variables)
         except Exception as ex:
             return Response({'detail': str(ex)}, status=HTTP_400_BAD_REQUEST)
 
         return Response(
             {
                 'result': result,
-                'variables': request.data.get('variables')
+                'variables': variables
             }
         )
