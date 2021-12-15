7d729d456bd804eeae51803e6d3e617846d9ce1f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 19:55:04 +0530

    Use `api_view` to disallow invalid request method

--- api/views.py
+++ api/views.py
@@ -1,11 +1,10 @@
 from django.http import (HttpRequest, HttpResponse, JsonResponse,
     HttpResponseNotAllowed)
+from rest_framework.decorators import api_view
 
 
+@api_view(['POST'])
 def evaluate(request: HttpRequest) -> HttpResponse:
     """Evaluate the expression contained in request and return it as JSON."""
 
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-    
     return JsonResponse({})
