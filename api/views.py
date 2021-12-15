752208d1fc40a134e3112cb7dc50f140fc5a1759
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 19:55:50 +0530

    Remove unused imports due to using `api_view`

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
-from django.http import (HttpRequest, HttpResponse, JsonResponse,
-    HttpResponseNotAllowed)
+from django.http import (HttpRequest, JsonResponse)
 from rest_framework.decorators import api_view
 
 
 @api_view(['POST'])
-def evaluate(request: HttpRequest) -> HttpResponse:
+def evaluate(request: HttpRequest) -> JsonResponse:
     """Evaluate the expression contained in request and return it as JSON."""
 
     return JsonResponse({})
