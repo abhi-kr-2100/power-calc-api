6b0b909a9c1f244e8627ef9b3c24053b41b76502
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 20:02:10 +0530

    Use rest_framework's Response instead of JsonResponse

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.http import (HttpRequest, JsonResponse)
+from django.http import HttpRequest
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
 @api_view(['POST'])
-def evaluate(request: HttpRequest) -> JsonResponse:
+def evaluate(request: HttpRequest) -> Response:
     """Evaluate the expression contained in request and return it as JSON."""
 
-    return JsonResponse({})
+    return Response(request.data)
