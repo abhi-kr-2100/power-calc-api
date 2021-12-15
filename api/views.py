035ba0784df5985123e3a3247c16790c6b542ff2
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-15 19:38:17 +0530

    Add a dummy `evaluate` view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.http.response import HttpResponseNotAllowed, JsonResponse
 
-# Create your views here.
+
+def evaluate(request: HttpRequest) -> JsonResponse:
+    """Evaluate the expression contained in request and return it as JSON."""
+
+    if request.method != 'POST':
+        return HttpResponseNotAllowed('Only POST allowed')
+    
+    return JsonResponse({})
